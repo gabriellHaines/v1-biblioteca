8154289824f1973376a9b22150dd5acfd5bec16b
gabriellHaines <gabriellhaines@gmail.com>
2023-05-19 16:42:56 -0300

    v1.1.1

---
+++ logado/funcionario/cadAutor.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Autor</title>
+</head>
+<body>
+    <h1>Cadastro de Autor</h1>
+    <form method = "post">
+    <label for="usuario">Digite seu Usuário: </label><br>
+        <input type="text" name='usuario'><br>
+        <label for="senha">Digite sua Senha: </label><br>
+        <input type="password" name='senha'><br>
+        <label for="senhaR">Repita sua Senha: </label><br>
+        <input type="password" name='senhaR'><br>
+        <label for="nome">Digite seu Nome: </label><br>
+        <input type="text" name='nome'><br>
+        <label for="celular">Digite seu Celular: </label><br>
+        <input type="text" name='celular'><br>
+        <label for="data_nascimento">Digite sua Data de Nascimento: </label><br>
+        <input type="text" name='data_nascimento'><br>
+        <label for="cpf">Digite seu CPF: </label><br>
+        <input type="text" name='cpf'><br>
+        <label for="endereco">Digite seu Endereço: </label><br>
+        <input type="text" name='endereco'><br>
+        <label for="nascionalidade">Digite sua Nascionalidade: </label><br>
+        <input type="text" name = "nascionalidade"><br>
+        <label for="biografia">Digite sua Biografia: </label><br>
+        <input type="text" name = "biografia"><br>
+
+        <button name = "enviar">Cadastrar</button>
+    </form>
+</body>
+</html>
